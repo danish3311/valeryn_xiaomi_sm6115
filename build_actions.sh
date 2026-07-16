99cfafb50d8083c0223d01debdcb4fc5f0958dc4
Danish Ali <danish3311@gmail.com>
2026-07-16 15:28:57 +0500

    Modify build_actions.sh to use new kernel repository
    Updated the script to clone a different repository for the kernel and removed the build steps for vanilla and ksun1.

--- build_actions.sh
+++ build_actions.sh
@@ -3,15 +3,12 @@
 
 set -x
 
-# Setup
-curl -o build_vanilla.sh -L https://raw.githubusercontent.com/Joe7500/valeryn_xiaomi_sm6115/refs/heads/build-actions/build_vanilla.sh || exit 1
-curl -o build_ksun1.sh -L https://raw.githubusercontent.com/Joe7500/valeryn_xiaomi_sm6115/refs/heads/build-actions/build_ksun1.sh || exit 1
 curl -o build_ksun3.sh -L https://raw.githubusercontent.com/Joe7500/valeryn_xiaomi_sm6115/refs/heads/build-actions/build_ksun3.sh || exit 1
 
 mkdir build
 cd build
 rm -rf kernel
-git clone https://github.com/Joe7500/valeryn_xiaomi_sm6115 kernel
+git clone https://github.com/danish3311/valeryn_xiaomi_sm6115 kernel
 
 cd kernel || exit 1
 KERNEL_BUILD_DIR=`pwd`
@@ -22,39 +19,6 @@ cd build/kernel || exit 1
 git config user.email "user@localhost"
 git config user.name "user"
 
-# vanilla
-rm -rf *
-git reset --hard
-git switch android16
-git switch -c build-vanilla || exit 1
-# mglru
-git cherry-pick 8b266fd430de31b1cd49049f9a7543516b223a6d || exit 
-git revert d0d19501b373a540fbd56551b430619e2d67b5a8 --no-edit || exit
-git cherry-pick d749fbbfb5e51e845e3ed227de64eba4ad80f861 || exit
-bash  ../../build_vanilla.sh 
-if [ $? -eq 0 ] ; then
-   cp 4.19*.zip ../../4.19-A16-valeryn-`date +'%d.%m.%y-%H%M'`.zip
-else
-   echo FAILED
-   exit 1
-fi
-
-# ksun1
-rm -rf *
-git reset --hard
-git switch android16
-git switch -c build-ksun1 || exit 1
-git cherry-pick 8b266fd430de31b1cd49049f9a7543516b223a6d || exit 
-git revert d0d19501b373a540fbd56551b430619e2d67b5a8 --no-edit || exit
-git cherry-pick d749fbbfb5e51e845e3ed227de64eba4ad80f861 || exit
-bash ../../build_ksun1.sh
-if [ $? -eq 0 ] ; then
-   cp 4.19*.zip ../../4.19-A16-valeryn-ksun-1.1.1-`date +'%d.%m.%y-%H%M'`.zip
-   cp kernel-prebuilt*.tar.xz ../../
-else
-   echo FAILED
-   exit 1
-fi
 
 # ksun3
 rm -rf *
